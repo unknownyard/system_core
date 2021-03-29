4225dcea60d8f7fdc302ffb9d3d3a5a7ffeef88d
spkal01 <kalligeross@gmail.com>
2021-03-29 13:17:04 +0530

    Revert "fastboot: Skip super partition update on devices with dynamic prebuil…"
    This reverts commit 54513e054e89ee6504e28d3ee9b2f1d7c6a31668.

--- fastboot/fastboot.cpp
+++ fastboot/fastboot.cpp
@@ -1347,11 +1347,6 @@ void FlashAllTool::Flash() {
     // or in bootloader fastboot.
     FlashImages(boot_images_);
 
-#ifdef TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS
-    if (!is_userspace_fastboot()) {
-        reboot_to_userspace_fastboot();
-    }
-#else
     // Sync the super partition. This will reboot to userspace fastboot if needed.
     UpdateSuperPartition();
 
@@ -1365,7 +1360,6 @@ void FlashAllTool::Flash() {
         };
         do_for_partitions(image->part_name, slot, resize_partition, false);
     }
-#endif
 
     // Flash OS images, resizing logical partitions as needed.
     FlashImages(os_images_);
